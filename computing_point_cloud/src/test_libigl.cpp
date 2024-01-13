38506561938b54bb993c76e8c73de8d1126a0fe8
Matteo Dalle Vedove <matteodv99tn@gmail.com>
2024-01-13 14:35:12 +0100

    Modified CmakeLists and update example script

--- computing_point_cloud/src/test_libigl.cpp
+++ computing_point_cloud/src/test_libigl.cpp
@@ -1,12 +1,12 @@
-#include <igl/readSTL.h>
-#include <igl/intersect_ray_mesh.h>
-
 #include <Eigen/Core>
+#include <fstream>
+#include <igl/ray_mesh_intersect.h>
+#include <igl/readSTL.h>
 #include <iostream>
 #include <string>
-#include <fstream>
 
 #include "defines.hpp"
+#include "igl/Hit.h"
 
 int main() {
     std::string filename = PRJ_ROOT_DIR + std::string("/mattonella.stl");
@@ -25,14 +25,13 @@ int main() {
     Eigen::RowVector3d origin(0.0, 0.0, 0.0);
     Eigen::RowVector3d direction(0.0, 0.0, 1.0);
 
-    Eigen::VectorXi I;
-    Eigen::MatrixXd C;
-    igl::intersect_ray_mesh(origin, direction, V, F, I, C);
+    std::vector<igl::Hit> hits;
+    igl::ray_mesh_intersect(origin, direction, V, F, hits);
 
     // Check if there is an intersection
-    if (I.size() > 0) {
+    if (hits.size() > 0) {
         // Print the coordinates of the first intersection point
-        std::cout << "First Intersection point: " << C.row(0) << std::endl;
+        std::cout << "First Intersection point: " << hits.front().v << std::endl;
     } else {
         std::cout << "No intersection." << std::endl;
     }
